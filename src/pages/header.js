c01b080f29a05ac4c64c4c7b2324b035f20e46db
Jesus Ruiz <hesus.ruiz@gmail.com>
2021-07-12 16:53:54 +0200

    deploy

--- src/pages/header.js
+++ src/pages/header.js
@@ -16,18 +16,18 @@ export class HeaderBar extends LitElement {
 
     render() {
         const sidebarMenu = html`
-        <div class="w3-bar w3-xlarge w3-indigo">
-          <a href="#" class="w3-bar-item w3-button">SafeIsland</a>
-          <a href="#" class="w3-bar-item w3-button w3-hide-small">Link 1</a>
-          <a href="#" class="w3-bar-item w3-button w3-hide-small">Link 2</a>
-          <a href="#" class="w3-bar-item w3-button w3-hide-small">Link 3</a>
-          <a href="javascript:void(0)" class="w3-bar-item w3-button w3-hover-indigo w3-right w3-hide-large w3-hide-medium" @click=${this.toggleView}>&#9776;</a>
+        <div class="w3-bar w3-xlarge w3-indigo w3-hover-indigo">
+          <a href="#" class="w3-bar-item w3-btn">SafeIsland</a>
+          <a href="#" class="w3-bar-item w3-btn w3-hide-small">Link 1</a>
+          <a href="#" class="w3-bar-item w3-btn w3-hide-small">Link 2</a>
+          <a href="#" class="w3-bar-item w3-btn w3-hide-small">Link 3</a>
+          <a href="javascript:void(0)" class="w3-bar-item w3-btn w3-right w3-hide-large w3-hide-medium" @click=${this.toggleView}>&#9776;</a>
         </div>
         
         <div id="vertMenu" class="w3-bar-block w3-indigo w3-hide w3-hide-large w3-hide-medium">
-          <a href="#" class="w3-bar-item w3-button">Link 1</a>
-          <a href="#" class="w3-bar-item w3-button">Link 2</a>
-          <a href="#" class="w3-bar-item w3-button">Link 3</a>
+          <a href="#" class="w3-bar-item w3-btn">Link 1</a>
+          <a href="#" class="w3-bar-item w3-btn">Link 2</a>
+          <a href="#" class="w3-bar-item w3-btn">Link 3</a>
         </div>
         `
         return sidebarMenu;
