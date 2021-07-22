515be8cabf3070e0dd0b1da3e4fc2c983997493c
Jesus Ruiz <hesus.ruiz@gmail.com>
2021-07-22 23:46:29 +0200

    deploy

--- docs/pages/spinner.js
+++ docs/pages/spinner.js
@@ -9,17 +9,11 @@ export class Spinner extends AbstractPage {
   enter(pageData) {
     console.log("SPINNER: enter page");
     let theHtml = html`
-        <div class="container">
-            <div class="w3-card-4 w3-center" style="margin-top:100px;">
-        
-                <div class="loaderpage" style="text-align: center;">
-                    <h2>Loading</h2>
-                    <p>Please wait...</p>
-                    <div class="loader"></div>
-                </div>
-        
-            </div>
-        </div>
+        <div class="loaderpage">
+            <h2>Loading</h2>
+            <p>Please wait...</p>
+            <div class="loader"></div>
+        </div>        
         `;
     this.render(theHtml);
     window.location.reload();
