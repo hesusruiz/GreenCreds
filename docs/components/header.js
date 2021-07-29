ec0915f7a70338fba0525f185b364c7570f213be
Jesus Ruiz <hesus.ruiz@gmail.com>
2021-07-29 15:10:16 +0200

    deploy

--- docs/components/header.js
+++ docs/components/header.js
@@ -23,18 +23,19 @@ export function HeaderBar() {
     x.classList.remove("w3-show");
   }
   let theHtml = html`
-    <div class="w3-bar w3-xlarge color-primary">
+    <div class="w3-bar w3-xlarge color-white">
         <img class="w3-bar-item" style="padding:0" @click=${goHome} href="javascript:void(0)" src="img/evidenceLogo60.png" alt="">
-        <a href="#" class="w3-bar-item w3-button w3-hide-small">${T("Privacy policy")}</a>
-        <a href="#" class="w3-bar-item w3-button  w3-hide-small">${T("Terms & Conditions")}</a>
-        <a href="javascript:void(0)" class="w3-bar-item w3-button hover-color-primary w3-right w3-hide-large w3-hide-medium" @click=${toggleMenu}>&#9776;</a>
+        <a @click=${() => gotoPage("selectLanguage")} href="javascript:void(0)" class="w3-bar-item btn-menu w3-large w3-hide-small w3-hide-medium">${T("Language")}</a>
+        <a href="#" class="w3-bar-item btn-menu w3-large w3-hide-small w3-hide-medium">${T("Privacy policy")}</a>
+        <a href="#" class="w3-bar-item btn-menu w3-large w3-hide-small w3-hide-medium">${T("Terms & Conditions")}</a>
+        <a href="javascript:void(0)" class="w3-bar-item btn-menu w3-right w3-hide-large" @click=${toggleMenu}>&#9776;</a>
     </div>
     
-    <div class="w3-bar-block color-primary w3-hide w3-hide-large w3-hide-medium">
-        <a @click=${() => gotoPage("spinner")} href="javascript:void(0)" class="w3-bar-item w3-button hover-color-primary">${T("Update public keys")}</a>
-        <a @click=${() => gotoPage("selectLanguage")} href="javascript:void(0)" class="w3-bar-item w3-button hover-color-primary">${T("Language")}</a>
-        <a href="#" class="w3-bar-item w3-button hover-color-primary">${T("Privacy policy")}</a>
-        <a href="#" class="w3-bar-item w3-button hover-color-primary">${T("Terms & Conditions")}</a>
+    <div class="w3-bar-block color-white w3-hide w3-hide-large">
+        <a @click=${() => gotoPage("spinner")} href="javascript:void(0)" class="w3-bar-item btn-menu">${T("Update public keys")}</a>
+        <a @click=${() => gotoPage("selectLanguage")} href="javascript:void(0)" class="w3-bar-item btn-menu">${T("Language")}</a>
+        <a href="#" class="w3-bar-item btn-menu">${T("Privacy policy")}</a>
+        <a href="#" class="w3-bar-item btn-menu">${T("Terms & Conditions")}</a>
     </div>
     `;
   render(theHtml, domElem);
